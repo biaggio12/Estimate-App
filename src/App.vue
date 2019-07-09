88e670c9bd7651716ac6dde28aab382a3763e4b0
Blazej Szmit <biaggio12@gmail.com>
2019-07-09 10:56:55 +0200

    Little bit of initial commit and Navbar Combponent fixed

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NavBar />
+    <Content />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import NavBar from './components/NavBar.vue'
+import Content from './components/Content.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    NavBar,
+    Content,
+  },
+  data: function() {
+      return {}
   }
 }
 </script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,46 @@
+<template>
+  <nav>
+    <ul id="nav">
+      <li v-for="item in navItems"><a href="#">{{item}}</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  data: function(){
+    return {
+        navItems: ['Sklep Internetowy','Strona Internetowa','Aplikacja internetowa','Blog','One Page'],
+            }
+                  },
+  methods: {
+    },
+  }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+#nav {
+  list-style:none;
+  display:flex;
+  flex-direction:row;
+  justify-content:flex-start;
+  background:#333333;
+  margin:5px 12px;
+  padding:0px;
+}
+#nav li a {
+  display:block;
+  padding:10px 22px;
+  color:#ffffff;
+  text-decoration:none;
+}
+#nav li a:hover {
+  background-color: #111111;
+}
+.active {
+  background-color: #4CAF50;
+}
+</style>
